61983db05d05e64e121011bb2abe558ea776872d
HDJGJonas <jonas.haeberli@hispeed.ch>
2024-03-03 19:08:14 +0100

    fix naming

---
+++ launch/moveit_gazebo.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+
+
+def generate_launch_description():
+
+    mercury_description_path = os.path.join(
+        get_package_share_directory('tr_robot_description'))
+
+    xacro_file = os.path.join(mercury_description_path,
+                              'urdf',
+                              'mercury_gazebo.urdf.xacro') 
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description_config = doc.toxml()
+    robot_description = {'robot_description': robot_description_config}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'robot'],
+                        output='screen')
+
+
+    return LaunchDescription([
+        spawn_entity,
+        node_robot_state_publisher,
+    ])
