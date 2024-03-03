fbdf53d60bc83bffc27cc63663f4e4206e2342df
HDJGJonas <jonas.haeberli@hispeed.ch>
2024-03-03 19:09:41 +0100

    fix naming

--- launch/ros2_control_gazebo.launch.py
+++ launch/ros2_control_gazebo.launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["position_trajectory_controller", "-c", "/controller_manager"],
+        arguments=["mercury_arm_controller", "-c", "/controller_manager"],
     )
 
 
